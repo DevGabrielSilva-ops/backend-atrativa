88f3728092b4bf906c6abd1571265d6a62df43bf
DevGabrielSilva-ops <dev13.gabriel@gmail.com>
2026-08-03 19:30:10 -0300

    feat: create prisma module and integrate database

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '@prisma/client/extension';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy, OnModuleInit {
+    constructor(){
+        const adapter = new PrismaMariaDb({
+            host: 'localhost',
+            user:'root',
+            password:'admin',
+            database: 'atrativaShore',
+            port: 3306,
+            connectionLimit: 5
+        })
+
+        super({ adapter })
+    }
+    
+    async onModuleInit() {
+        await this.$connect();
+    }
+    
+    async onModuleDestroy() {
+       await this.$disconnect();
+    }
+}
